3022d62ba3827dc9fdff176801aa0ec87bde0c62
spygeorgoulas <spygeorgoulas@gmail.com>
2025-08-20 00:54:12 +0300

    scalegmn renew

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -3,6 +3,9 @@ from .fmnist_inr_dataset import LabeledFashionMnistINRDataset
 from .cifar10_dataset import NFNZooDataset, CNNDataset
 from .cifar_inr_dataset import CifarINRDataset
 
+# spygeo
+from ._3d_inr_dataset import Labeled3DINRDataset
+
 
 def dataset(dataset_config, **kwargs):
     _map = {
@@ -11,6 +14,7 @@ def dataset(dataset_config, **kwargs):
         'cifar_inr': CifarINRDataset,
         'cifar10': NFNZooDataset,
         'svhn': NFNZooDataset,
-        'mnist_inr_edit': MNISTINRImageDataset
+        'mnist_inr_edit': MNISTINRImageDataset,
+        'labeled_3d_inr': Labeled3DINRDataset #edit
     }
     return _map[dataset_config["dataset"]](**dataset_config, **kwargs)
